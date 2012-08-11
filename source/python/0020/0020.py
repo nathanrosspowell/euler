dc7cbaf1480656010f36671565f21f827bcdaafc
Nathan Ross Powell <nathanrosspowell@gmail.com>
2012-08-11 01:01:20 -0400

    [ADD] 0020 in python

---
+++ source/python/0020/0020.py
@@ -0,0 +1,11 @@
+# n! means n  (n  1)  ...  3x2x1
+#
+# For example, 10! = 10x9x...x3x2x1 = 3628800,
+# and the sum of the digits in the number 10! is 
+# 3 + 6 + 2 + 8 + 8 + 0 + 0 = 27.
+#
+# Find the sum of the digits in the number 100!
+import operator
+print reduce(lambda x,y:int(x)+int(y),str(reduce(operator.mul,xrange(1,101))))
+
+
