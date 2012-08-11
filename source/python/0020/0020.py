2c01146cb250ae67948504b5646726d2b61f808a
Nathan Ross Powell <nathanrosspowell@gmail.com>
2012-08-11 10:15:43 -0400

    [MOD] update py 20, remove swap file

--- source/python/0020/0020.py
+++ source/python/0020/0020.py
@@ -1,11 +1,9 @@
 # n! means n  (n  1)  ...  3x2x1
 #
 # For example, 10! = 10x9x...x3x2x1 = 3628800,
-# and the sum of the digits in the number 10! is 
+# and the sum of the digits in the number 10! is
 # 3 + 6 + 2 + 8 + 8 + 0 + 0 = 27.
 #
 # Find the sum of the digits in the number 100!
 import operator
-print reduce(lambda x,y:int(x)+int(y),str(reduce(operator.mul,xrange(1,101))))
-
-
+print reduce(lambda x,y:int(x)+int(y),str(reduce(operator.mul,xrange(2,101))))
