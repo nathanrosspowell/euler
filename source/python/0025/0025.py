4d18e747d93c876884accf521af1f87ed92d09db
Nathan Ross Powell <nathanrosspowell@gmail.com>
2012-09-03 16:38:00 -0400

    [MOD] Removed the string conversion in 0025.py

--- source/python/0025/0025.py
+++ source/python/0025/0025.py
@@ -3,11 +3,10 @@
 # What is the first term in the Fibonacci sequence to contain 1000 digits?
 last = 1
 current = 1
-term = 3 
-while True:
+term = 2 
+fib = 2
+while fib < 10**999:
     fib = last + current
-    if len( str( fib ) ) == 1000:
-        break
     last = current
     current = fib
     term += 1 
