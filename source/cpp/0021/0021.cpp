a745905359bb625233496d5f0d745bbe0ab2ab0b
Nathan Ross Powell <nathanrosspowell@gmail.com>
2012-09-03 13:05:44 -0400

    [ADD] CPP 0021

---
+++ source/cpp/0021/0021.cpp
@@ -0,0 +1,45 @@
+// Let d(n) be defined as the sum of proper divisiors of 
+// n(numbers less than n which divide evenly into n).
+// If d(a) = b and d(b) = a where a =/= b, then a and b are an impicable pair
+// and each of a and b are called amiciable numbers.
+//
+// For example, the proper divisors of 220 are 1, 2, 4, 5, 10, 11, 20, 22, 44 
+// and 110; therefore d(220) - 284.
+// The proper divisors of 284 are 1, 2, 4, 71 and 142; so d(284) = 220.
+//
+// Evaluate the sum of all the amicable numbers under 10000.
+#include <iostream>
+
+unsigned int sum( unsigned int number )
+{   
+    unsigned int total = 0;
+    for ( unsigned int i = 1; i < number; ++i )
+    {
+        if ( number % i == 0 )
+        {
+            total += i;
+        }
+    }
+    return total;
+}
+
+int main()
+{
+    unsigned int totalSum = 0; 
+    unsigned int sums[ 10000 ] = {};
+    for ( unsigned int i = 0; i < 10000; ++i )
+    {
+        sums[ i ] = sum( i );
+    }
+    for ( unsigned int i = 0; i < 10000; ++i )
+    {
+        unsigned int number = sums[ i ];
+        if (  number < 10000 && sums[ number ] == i && number != i )
+        {
+            totalSum += number;
+        }
+    }
+    std::cout << totalSum << std::endl;
+    return 0;
+}
+
