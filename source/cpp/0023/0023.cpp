1eaded76cd6127d99fe8d355850e52162c856d1b
Nathan Ross Powell <nathanrosspowell@gmail.com>
2012-09-03 22:57:45 -0400

    [ADD] Works in progress

--- source/cpp/0023/0023.cpp
+++ source/cpp/0023/0023.cpp
@@ -1,91 +1,96 @@
-// A perfect numebr is a number for which the sum of it's proper devisors 
+// A perfect numebr is a number for which the sum of it's proper devisors
 // is exactly equal to the number.
 // For example, the sum of the proper divisors of 28 would be
 // 1 + 2 + 3 + 4 + 6 +14 = 28. which means 28 is a perfect number.
 //
-// A number N is called deficient if the sum of it's proper divisors is less 
+// A number N is called deficient if the sum of it's proper divisors is less
 // than N and it is called abundant if the sum exceeds N.
 //
 // As 12 is the smallest abundant number, 1 + 2 + 3 + 4 + 6 = 16, the smallest
 // number that can be written as the sum of two abundant numbers is 24. By
 // mathematical analysis, it can be shown that all integers greater than
-// 28123 can be writen as the sum of two abundant numbers. However, this 
+// 28123 can be writen as the sum of two abundant numbers. However, this
 // upper limit cannot be reduced any further by analysis even though it is
 // known that the geatest number than cannot be expressed as the sum of two
 // abundant numbers is less than this limit.
 //
-// Find the sum of all the positive integers which cannot be written as the 
+// Find the sum of all the positive integers which cannot be written as the
 // sum of two abundant numbers.
 #include <iostream>
 #include <vector>
+#include <numeric>
 #include <algorithm>
 
-typedef uint uint;
+typedef unsigned int uint;
 typedef std::vector< uint > List;
+
+uint sum( uint number )
+{   
+    uint total = 0;
+    List div;
+    for ( unsigned int i = 1; i <= number / 2; ++i )
+    {
+        if ( number % i == 0 )
+        {
+            if ( std::find( div.begin(), div.end(), i ) == div.end() )
+            {
+                div.push_back( i );
+            }
+        }
+    }
+    for ( List::iterator i = div.begin(); i != div.end(); ++ i )
+    {
+        total += *i;
+    }
+    return total;
+}
 
 int main()
 {
-    int limit = 28123;
+    //const uint limit = 20161;
+    const uint limit = 28123;
     List abundant;
-    for ( int i = 1; i < limit; ++i )
+    for ( uint num = 1; num < limit; ++num )
     {
-        uint total = 0;
-        for ( uint j = 1; j < i; ++j )
+        if ( sum( num ) > num )
         {
-            if ( i % j == 0 )
-            {
-                total += j;
-            }
-        }
-        if ( total > i )
-        {
-            abundant.push_back( i );
+            abundant.push_back( num );
         }
     }
+
+    std::cout << "Abundants: " << abundant.size() << std::endl;
+
+    std::vector< bool > fromPair( limit + 1 );
+    for ( int i = 0; i < fromPair.size(); ++i )
+    {
+        fromPair[ i ] = false;
+    }
+
+    for ( int i = 0; i < abundant.size(); ++i )
+    {
+        for ( int j = i; j < abundant.size(); ++j )
+        {
+            uint pair = abundant[ i ] + abundant[ j ];
+            if ( pair <= limit )
+            {
+                fromPair[ pair ] = true;
+            }
+        }
+    }
+
     uint totalSum = 0; 
-    for ( int i = 1; i < limit; ++i )
+    uint numnonPairs = 0;
+    for ( uint i = 0; i < abundant.size(); ++i )
     {
-        List::iterator end = abundant.end();
-        uint halfLimit = ( i / 2 ) + 1;
-        for ( uint j = 1; j < halfLimit; ++j )
+        uint a = abundant[ i ];
+        if ( !fromPair[ a ] )
         {
-            int a = j;
-            int b = i - j;
-            bool foundA = false, foundB = false;
-            List::iterator it = abundant.begin();
-            while ( it != end )
-            {
-                uint& val = *it;
-
-                if ( val == a ) 
-                {
-                    foundA = true;
-                }
-                if ( val == b )
-                {
-                    foundB = true;
-                }
-
-                if ( foundA && foundB )
-                {
-                    break;
-                }
-                else if ( val > halfLimit )
-                {
-                    end = it;
-                    break;
-                }
-                else
-                {
-                    ++it;
-                }
-            } 
-            if ( foundA && foundB )
-            {
-                totalSum += i;
-            }
+            totalSum += a;
+            ++numnonPairs; 
+            std::cout << "  " << a << std::endl;
         }
     }
+    std::cout << numnonPairs << std::endl;
     std::cout << totalSum << std::endl;
     return 0;
 }
---
+++ source/python/0026/0026.py
@@ -0,0 +1,42 @@
+def pattern( digits ):
+    if len( digits ) < 2:
+        return 0, []  
+    neg = -len( digits )
+    for x in xrange( -1, neg, -1 ):
+        y = x + x 
+        z = y + x
+        #print "x", x, "y", y, "z"
+        #print digits[ x :  ], digits[ y : x ], digits[ z : y ]
+        if digits[ x :  ] == digits[ y : x ] == digits[ z : y ]:
+            return len( digits[ x : ] ), digits
+    return 0, []
+
+def fraction( den ):
+    digits = []
+    num = 1
+    while True:
+        remain = num % den
+        div = ( num - remain ) / den
+        if div > 0:
+            digits.append( div )
+        if remain == 0:
+            break
+        num = remain * 10
+        pat = pattern( digits )
+        if pat[0] > 0 :
+            return pat
+    return 0,[] 
+
+print fraction( 983 )
+print 1/0
+length = 0
+number = 0
+for i in xrange( 2, 1001 ):
+    iLength, digits = fraction( i )
+    print i, iLength
+    if iLength > length:
+        length = iLength
+        number = i
+print number
+
+
