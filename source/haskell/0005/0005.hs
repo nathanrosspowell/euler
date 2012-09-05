d9e7f51aa139c8ad3f96f2c692554c064ae6dc80
Nathan Ross Powell <nathanrosspowell@gmail.com>
2012-09-05 19:02:17 -0400

    [MOD] Speed up of Haskell 0005
    Instead of increasing by 1 each time a number fails, go through
    and find the next multiple that will pass that mod test.

--- source/haskell/0005/0005.hs
+++ source/haskell/0005/0005.hs
@@ -1,12 +1,17 @@
 -- 2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
 -- What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
 main = print $ test 1 
+increase x y z
+    | n == 0    = z 
+    | otherwise = increase x y (z + 1)
+    where 
+        n = (x * z) `mod` y
 divide x y
-    | n > 0 = False
-    | y < 20 =  divide x (y + 1 )
-    | otherwise = True
+    | n > 0     = increase x y 2
+    | y < 20    = divide x (y + 1)
+    | otherwise = 0 
     where
         n = x `mod` y  
-test x = if n then x else test (x + 1) 
+test x = if inc == 0 then x else test (x * inc) 
     where
-        n = divide x 2 
+        inc = divide x 2 
