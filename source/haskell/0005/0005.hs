148239392cad216e772b46029e8a9559c019eb31
Nathan Ross Powell <nathanrosspowell@gmail.com>
2012-09-05 18:42:33 -0400

    [MOD] Actual correct test for Haskell 0005
    This meant the solution was very slow, so I modified it to
    bail after the first failed mod check. It's just over 60 seconds
    (79 seconds) on my netbook at the moment. Further prime filtering
    is required, but I'm not sure how to do that atm.

--- source/haskell/0005/0005.hs
+++ source/haskell/0005/0005.hs
@@ -1,6 +1,12 @@
 -- 2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
 -- What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
-main = print $ divide 1 
-divide x = if n == 0 then x else divide (x + 1) 
+main = print $ test 1 
+divide x y
+    | n > 0 = False
+    | y < 20 =  divide x (y + 1 )
+    | otherwise = True
     where
-        n = foldr (+) 0 $ map (x `mod`) [2..10]  
+        n = x `mod` y  
+test x = if n then x else test (x + 1) 
+    where
+        n = divide x 2 
