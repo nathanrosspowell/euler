26ca09b236fdc8fa312d142d6c86281d4aeab2fd
Nathan Ross Powell <nathanrosspowell@gmail.com>
2012-09-06 22:11:38 -0400

    [MOD] Speed up Python 0014
    Read the forums and got the idea to implement a hash of all past
    sequence numbers. Added this to vastly speed up the times and also
    remove the i % 2 check in the loop... which I had no idea what it
    was there for, and there is no proof to suggest that the answer
    could not have come from an even number.

--- source/python/0014/0014.py
+++ source/python/0014/0014.py
@@ -4,23 +4,22 @@
 #      n -> 3n+1 (n is odd)
 #
 # Which starting number, under one million, produces the longes chain?
-
+store = {}
 def sequence( num ):
-    count = 0
-    while num > 1:
-        count += 1
-        if num % 2 == 0:
-            num = num / 2
-        else:
-            num = 3 * num + 1
-    return count
-
-biggestChain = 0
-biggestChainNum = 0
+    if num <= 1:
+        return 1 
+    elif num % 2 == 0:
+        num = num / 2
+    else:
+        num = 3 * num + 1
+    if not store.has_key( num ):
+        store[ num ] = sequence( num )
+    return 1 + store[ num ] 
+chain = 0
+chainNum = 0
 for i in xrange( 1000000 ):
-    if i % 2:
-        chain = sequence( i )
-        if chain > biggestChain:
-            biggestChain = chain
-            biggestChainNum = i
-print biggestChainNum
+    result = sequence( i )
+    if result > chain:
+        chain = result
+        chainNum = i
+print chainNum
