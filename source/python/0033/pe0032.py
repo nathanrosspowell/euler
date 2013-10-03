3fafd0a19090a7724e54ca9ad5fc982446c41486
Nathan Ross Powell <nathanrosspowell@gmail.com>
2013-10-03 13:42:27 -0400

    [ADD] WIP 0033

---
+++ source/python/0033/pe0032.py
@@ -0,0 +1,25 @@
+# The fraction 49/98 is a curious fraction, as an inexperienced 
+# mathematician in attempting to simplify it may incorrectly believe 
+# that 49/98 = 4/8, which is correct, is obtained by cancelling the 9s.
+#
+# We shall consider fractions like, 30/50 = 3/5, to be trivial examples.
+#
+# There are exactly four non-trivial examples of this type of fraction,
+# less than  one in value, and containing two digits in the numerator 
+# and denominator.
+
+# If the product of these four fractions is given in its lowest common 
+# terms, find the value of the denominator.
+def divide( x, y ):
+    return ( x * 1.0 ) / ( y * 1.0 )
+def getVariations( num ):
+    for i in xrange( 1, num ):
+        for j in xrange( 1, num ):
+            yield ( i*10 + num, num*10 + j ), ( i, j )
+results = []
+for i in range( 1, 10 ):
+    for calc, test in getVariations( i ):
+        #print "Test:",calc, test
+        result = divide( *calc )
+        if result == divide( *test ):
+            results.append( test )
