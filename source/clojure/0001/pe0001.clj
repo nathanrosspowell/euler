ae8f38462afc5210a4dfeff13181a8f9f300811f
Nathan Ross Powell <nathanrosspowell@gmail.com>
2013-10-30 13:06:09 -0400

    [MOD] used OR to joing the ifs

--- source/clojure/0001/pe0001.clj
+++ source/clojure/0001/pe0001.clj
@@ -1,1 +1,1 @@
-(reduce + (map (fn [i] (if (= (mod i 3) 0) i (if (= (mod i 5) 0) i 0))) (for [i (range 999)] (inc i)))))
+(reduce + (map (fn [i] (if (or (= (mod i 3) 0) (= (mod i 5) 0)) i 0)) (for [i (range 999)] (inc i))))
